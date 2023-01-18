2a526ece78efe9fb0f1c3e4627fe807874ea7593
Alexander Sparkowsky <info@roamingthings.de>
2023-01-18 19:46:37 +0100

    Add cli arguments

--- spyglass.py
+++ spyglass.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
 
-# Mostly copied from https://picamera.readthedocs.io/en/release-1.13/recipes2.html
-# Run this script, then point a web browser at http:<this-ip-address>:8000
-# Note: needs simplejpeg to be installed (pip3 install simplejpeg).
-
+import argparse
 import io
 import logging
+import re
 import socketserver
-import sys
 from http import server
 from threading import Condition
 
@@ -15,11 +12,6 @@ from picamera2 import Picamera2
 from picamera2.encoders import JpegEncoder
 from picamera2.outputs import FileOutput
 
-ip = sys.argv[1]
-port = int(sys.argv[2])
-width = int(sys.argv[3])
-height = int(sys.argv[4])
-fps = int(sys.argv[5])
 
 class StreamingOutput(io.BufferedIOBase):
     def __init__(self):
@@ -34,11 +26,7 @@ class StreamingOutput(io.BufferedIOBase):
 
 class StreamingHandler(server.BaseHTTPRequestHandler):
     def do_GET(self):
-        if self.path == '/':
-            self.send_response(301)
-            self.send_header('Location', '/stream.mjpg')
-            self.end_headers()
-        elif self.path == '/stream.mjpg':
+        if self.path == '/stream.mjpg':
             self.send_response(200)
             self.send_header('Age', 0)
             self.send_header('Cache-Control', 'no-cache, private')
@@ -70,13 +58,41 @@ class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
     daemon_threads = True
 
 
+def split_resolution(res):
+    parts = res.split('x')
+    w = int(parts[0])
+    h = int(parts[1])
+    return w, h
+
+
+def resolution_type(arg_value, pat=re.compile(r"^\d+x\d+$")):
+    if not pat.match(arg_value):
+        raise argparse.ArgumentTypeError("invalid value: <width>x<height> expected.")
+    return arg_value
+
+
+parser = argparse.ArgumentParser(
+    prog='Spyglass',
+    description='Start a webserver for Picamera2 videostreams.'
+)
+parser.add_argument('-b', '--bindaddress', type=str, default='', help='Bind to address for incoming connections')
+parser.add_argument('-p', '--port', type=int, default=8080, help='Bind to port for incoming connections')
+parser.add_argument('-r', '--resolution', type=resolution_type, default='640x480', help='Resolution of the images width x height')
+parser.add_argument('-f', '--fps', type=int, default=15, help='Frames per second to capture')
+
+args = parser.parse_args()
+bind_address = args.bindaddress
+port = args.port
+width, height = split_resolution(args.resolution)
+fps = args.fps
+
 picam2 = Picamera2()
 picam2.configure(picam2.create_video_configuration(main={"size": (width, height)}, controls={"FrameRate": (fps, fps)}))
 output = StreamingOutput()
 picam2.start_recording(JpegEncoder(), FileOutput(output))
 
 try:
-    address = ('', port)
+    address = (bind_address, port)
     server = StreamingServer(address, StreamingHandler)
     server.serve_forever()
 finally:
