c84c6a8394e81d909b5607b0b81b0b558dd15f1b
Alexander Sparkowsky <info@roamingthings.de>
2023-03-11 21:37:59 +0100

    feat: create exif header only separately
    Signed-off-by: Alexander Sparkowsky <info@roamingthings.de>

--- spyglass/server.py
+++ spyglass/server.py
@@ -3,6 +3,8 @@ import logging
 import socketserver
 from http import server
 from threading import Condition
+
+from spyglass.exif import create_exif_header
 from . import logger
 
 
@@ -22,8 +24,10 @@ class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
     daemon_threads = True
 
 
-def run_server(bind_address, port, output, stream_url='/stream', snapshot_url='/snapshot', orientation_exif = 0):
-    class StreamingHandler(server.BaseHTTPRequestHandler):        
+def run_server(bind_address, port, output, stream_url='/stream', snapshot_url='/snapshot', orientation_exif=0):
+    exif_header = create_exif_header(orientation_exif)
+
+    class StreamingHandler(server.BaseHTTPRequestHandler):
         def do_GET(self):
             if self.path == stream_url:
                 self.start_streaming()
@@ -44,14 +48,13 @@ def run_server(bind_address, port, output, stream_url='/stream', snapshot_url='/
                         output.condition.wait()
                         frame = output.frame
                     self.wfile.write(b'--FRAME\r\n')
-                    if orientation_exif <= 0:
+                    if exif_header is None:
                         self.send_jpeg_content_headers(frame)
                         self.end_headers()
                         self.wfile.write(frame)
                         self.wfile.write(b'\r\n')
                     else:
-                        exif_header = self.get_orientation_exif_header(orientation_exif)
-                        self.send_jpeg_content_headers(frame, len(exif_header)-2)
+                        self.send_jpeg_content_headers(frame, len(exif_header) - 2)
                         self.end_headers()
                         self.wfile.write(exif_header)
                         self.wfile.write(frame[2:])
@@ -71,8 +74,7 @@ def run_server(bind_address, port, output, stream_url='/stream', snapshot_url='/
                     self.end_headers()
                     self.wfile.write(frame)
                 else:
-                    exif_header = self.get_orientation_exif_header(orientation_exif)
-                    self.send_jpeg_content_headers(frame, len(exif_header)-2)
+                    self.send_jpeg_content_headers(frame, len(exif_header) - 2)
                     self.end_headers()
                     self.wfile.write(exif_header)
                     self.wfile.write(frame[2:])
@@ -88,18 +90,7 @@ def run_server(bind_address, port, output, stream_url='/stream', snapshot_url='/
 
         def send_jpeg_content_headers(self, frame, extra_len=0):
             self.send_header('Content-Type', 'image/jpeg')
-            self.send_header('Content-Length', len(frame)+extra_len)
-
-        def get_orientation_exif_header(self, orientation_exif):
-            return b''.join([
-                    b'\xFF\xD8\xFF\xE1\x00\x62\x45\x78\x69\x66\x00\x00\x4D\x4D\x00\x2A',
-                    b'\x00\x00\x00\x08\x00\x05\x01\x12\x00\x03\x00\x00\x00\x01\x00',
-                    (orientation_exif).to_bytes(1, 'big'),
-                    b'\x00\x00\x01\x1A\x00\x05\x00\x00\x00\x01\x00\x00\x00\x4A\x01\x1B',
-                    b'\x00\x05\x00\x00\x00\x01\x00\x00\x00\x52\x01\x28\x00\x03\x00\x00',
-                    b'\x00\x01\x00\x02\x00\x00\x02\x13\x00\x03\x00\x00\x00\x01\x00\x01',
-                    b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x48\x00\x00\x00\x01\x00\x00',
-                    b'\x00\x48\x00\x00\x00\x01'])
+            self.send_header('Content-Length', str(len(frame) + extra_len))
 
     logger.info('Server listening on %s:%d', bind_address, port)
     logger.info('Streaming endpoint: %s', stream_url)
