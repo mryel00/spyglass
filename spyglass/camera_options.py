42ccc6066cb8737000becf0d7e6d1b503c3f1586
Patrick Gehrsitz <mryel00.github@gmail.com>
2024-02-28 15:08:04 +0100

    refactor: add css style
    css base provided by roamingthings.
    
    Signed-off-by: Patrick Gehrsitz <mryel00.github@gmail.com>

--- spyglass/camera_options.py
+++ spyglass/camera_options.py
@@ -1,55 +1,53 @@
 def parse_dictionary_to_html_page(camera, parsed_controls='None', processed_controls='None'):
-    html = """
-            <html>
+    html =  """
+            <!DOCTYPE html>
+            <html lang="en">
+            """
+    html += f"""
                 <head>
-                    <title>Spyglass camera options</title>
-                    <style>
-                        table {
-                            border-collapse: collapse;
-                            width: 100%;
-                        }
-                        
-                        th, td {
-                            border: 1px solid black;
-                            padding: 8px;
-                            text-align: left;
-                        }
-                        
-                        tr:nth-child(even) {
-                            background-color: #f2f2f2;
-                        }
-                    </style>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <title>Camera Settings</title>
+                    <style>{get_style()}</style>
                 </head>
             """
-    html+= f"""
+    html += f"""
                 <body>
                     <h1>Available camera options</h1>
                     <h3>Parsed Controls: {parsed_controls}</h3>
                     <h3>Processed Controls: {processed_controls}</h3>
-                    <table>
-                        <tr>
-                            <th>Option</th>
-                            <th>Min value</th>
-                            <th>Max value</th>
-                            <th>Default value</th>
-                        </tr>
-        """
+            """
     for control,values in camera.camera_controls.items():
         html += f"""
-                        <tr>
-                            <td>{control}</td>
-                            <td>{values[0]}</td>
-                            <td>{values[1]}</td>
-                            <td>{values[2]}</td>
-                        </tr>
+                    <div class="card-container">
+                        <div class="card">
+                            <h2>{control}</h2>
+                            <div class="card-content">
+                            <div class="setting">
+                                <span class="label">Min:</span>
+                                <span class="value">{values[0]}</span>
+                            </div>
+                            <div class="setting">
+                                <span class="label">Max:</span>
+                                <span class="value">{values[1]}</span>
+                            </div>
+                            <div class="setting">
+                                <span class="label">Default:</span>
+                                <span class="value">{values[2]}</span>
+                            </div>
+                            </div>
+                        </div>
+                    </div>
                 """
     html += """
-                    </table>
                 </body>
             </html>
             """
     return html
 
+def get_style():
+    with (open('resources/controls_style.css', 'r')) as f:
+        return f.read()
 
 def process_controls(camera, controls):
     controls_dict = camera.camera_controls
